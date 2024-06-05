fdd2657bd55375ea5b2e060bc1f1f8fe913a6b92
veera <veera@Veerasekhar>
2024-06-05 22:59:16 +0530

    JAVA BASIC

---
+++ src/java_basic/print_name.java
@@ -0,0 +1,13 @@
+package java_basic;
+//Printing name
+public class print_name {
+	
+	public static void main(String[] args) {
+		 //fields are used to store data
+		String name="shiva";
+		//take refer name 
+		System.out.println(name);
+		// directly print name
+		System.out.println("veerasekhar");
+	}
+}
